053efc2dd592cc12ba2d3b29a293e0ba596e1394
Manan Sehgal <manansehgal97@gmail.com>
2025-09-01 13:18:19 +0530

    app.jsx issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Home from './Pages/Home'
 import Footer from './Components/Footer/Footer'
-import BookCard from './Components/bookCard'
-import Store from './Pages/Store'
 
 function App() {
   const [count, setCount] = useState(0)
